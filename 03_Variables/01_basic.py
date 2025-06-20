ffbf6f06504d3823452f75f1f8adcd625a90be6a
pansh30 <ansh.patel30092004@gmail.com>
2025-06-20 10:41:07 +0530

    intial commit

---
+++ 03_Variables/01_basic.py
@@ -0,0 +1,12 @@
+age = 20 #integer
+name = "Ansh" #string
+cgpa = 8.69 #float
+
+
+__age_7 = 34 #Valid becuase underscore are allow.
+
+# ag$$e = 34  Invalid becuse not contain special char.
+
+# 4age = 34 #Invalid because cannot start with Numbers.
+
+
---
+++ 03_Variables/02_data_types.py
@@ -0,0 +1,26 @@
+# Python supports several built-in data types:
+#     Integers (int): Whole numbers (e.g., 10, -5).
+#     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+#     Booleans (bool): Represents True or False.
+#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+#     Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+age = 34
+print(age)
+print(type(age))
+
+
+name= "ansh"
+print(name)
+print(type(name))
+
+cgpa = 8.69
+print(cgpa)
+print(type(cgpa))
+
+is_complete = True # can also False
+print(is_complete)
+print(type(is_complete))

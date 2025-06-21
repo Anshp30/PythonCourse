2aeb18a53fd55e5e7dab29a60162ac74bcb4f056
pansh30 <ansh.patel30092004@gmail.com>
2025-06-21 10:57:45 +0530

    intial commit

---
+++ 04_Typecasting/02_example.py
@@ -0,0 +1,16 @@
+#conert str to an int
+
+num_str = "10"
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+#conert float to an int
+
+num_float = 3.14
+print(num_float)
+print(type(num_float))
+
+pi = int(num_float)
+print(pi)
+print(type(pi))

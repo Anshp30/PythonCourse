122aea9541447d665c401735f79f1032217c816b
pansh30 <ansh.patel30092004@gmail.com>
2025-06-21 11:19:26 +0530

    intial commit

---
+++ 05_userinput/01_input.py
@@ -0,0 +1,5 @@
+a = input("Enter Your Name :")
+print(a)
+
+b = input("Enter Your Age :")
+print(b)
---
+++ 05_userinput/02_add.py
@@ -0,0 +1,9 @@
+# a = input("Enter First Number:")
+# a= int(a) # convert to an int version
+# print(a + 3)
+
+a = int(input("Enter first Number:"))
+# a = int(a)
+b = int(input("Enter Second Number:"))
+# b = int(b)
+print(a+b)

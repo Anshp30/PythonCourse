8e03761397f93b8a751495051c6fc56be645ad32
pansh30 <ansh.patel30092004@gmail.com>
2025-06-25 10:26:07 +0530

    intial commit

---
+++ 07_operators/03_Logical_operator.py
@@ -0,0 +1,24 @@
+#Logical opertor
+# And, or, Not
+
+c = True
+d = False
+
+print(True and True)
+print(True and False)
+print(False and True) 
+print(False and False)
+
+
+print("For or operarors:")
+print(True or True)
+print(True or False)
+print(False or True) 
+print(False or False)
+
+print("For not operarors:")
+print(not(True))
+print(not(False))
+
+
+

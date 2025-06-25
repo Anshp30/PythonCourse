dab5cc0c1749b8d8fa7f5ca62e9e38cd98551687
pansh30 <ansh.patel30092004@gmail.com>
2025-06-25 10:27:01 +0530

    intial commit

---
+++ 07_operators/02_conditinal_oprator.py
@@ -0,0 +1,10 @@
+#conditional oprator
+a = 34
+b = 2 
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4)# Is A equal to 4?
+print(a==34)# Is A equal to 34?
+print(a!=34)# Is A Notequal to 34?

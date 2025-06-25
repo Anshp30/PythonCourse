bd104aca55512901d39a4e42a6928f16c869ee30
pansh30 <ansh.patel30092004@gmail.com>
2025-06-25 16:35:43 +0530

    intial commit

--- 07_operators/01_Airthmathic_operator.py
+++ 07_operators/01_Airthmathic_operator.py
@@ -3,6 +3,7 @@ a = 34
 b = 2
 
 # Arithmathic Operator
+
 print("a + b =",a + b)
 print("a - b =",a - b)
 print("a * b =",a * b)
---
+++ 09_Loop/01_for_loops.py
@@ -0,0 +1,9 @@
+# for i in range(1,6):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# Table of 5
+for i in range(1,11):
+    print("5 x",i, "=", 5*i) 

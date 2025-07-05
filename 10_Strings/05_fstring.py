26f8a93569beebb8a42de1714a7c3a0a0104e5c2
pansh30 <ansh.patel30092004@gmail.com>
2025-07-05 15:46:45 +0530

    intial commit

--- 10_Strings/05_fstring.py
+++ 10_Strings/05_fstring.py
@@ -15,3 +15,20 @@ print(s1)
 print(f"Dear {a},You are awesome.take this {a1}$ bag.") 
 print(f"Dear {b},You are awesome.take this {b1}$ bag.") 
 print(f"Dear {c},You are awesome.take this {c1}$ bag.") 
+
+
+
+name = "Ansh"
+age = 20
+
+print(f"My Name is {name},I am {age} Year Old!!")
+
+x = 10
+y = 5
+
+print(f"The sum of {x} and {y} is {x+y}")
+
+text = "python"
+print(f"{text:<10}") # Left Align 
+print(f"{text:>10}") # Right Align
+print(f"{text:^10}") # Cente Align

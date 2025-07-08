b8bff927f5cb3473224322827b2bb335ea8f6b29
pansh30 <ansh.patel30092004@gmail.com>
2025-07-08 11:29:00 +0530

    Intial commit

---
+++ PROJECT/Project1.py
@@ -0,0 +1,26 @@
+# Shopping Cart
+ 
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food you want to buy(q to quite): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter a price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("----- YOUR CART -----")
+
+for food in foods:
+    print(food, end= " ")
+
+for price in prices:
+    total = total + price
+
+print()
+print(f"--YOUR TOTAL IS : ${total}  ")
+print("THANK YOU FOR VISITING!")

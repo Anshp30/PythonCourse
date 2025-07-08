9b6feb065011ed1448e3c9af357a9dbb9594d63b
pansh30 <ansh.patel30092004@gmail.com>
2025-07-08 10:52:51 +0530

    intial commit

--- 11_Functions/02_function_arguments.py
+++ 11_Functions/02_function_arguments.py
@@ -1,3 +1,5 @@
+#Positional argument 
+
 def add(a,b,plus=0):
     x = a + b + plus
     return x
@@ -5,5 +7,15 @@ def add(a,b,plus=0):
 c = add(4,3,3)
 print(c)
 
+#Default argument
 
+def greet(name="Ansh"):
+    return f"Hello,{name}!"
+print(greet())
+
+#Keyword argument 
+
+def student(name,age):
+    return f"Name:{name} , Age:{age}"
+print(student(age = 20, name = "Ansh"))
 
